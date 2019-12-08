7ba3f60f10e899fedd51f89a2daffdd2f9ae3907
carlosaqf <carlosfegurgur@gmail.com>
2019-12-08 15:48:57 -0600

    Edited Button to conform to Material UI guidelines.

--- src/Components/button.js
+++ src/Components/button.js
@@ -1,10 +1,31 @@
 import styled from 'styled-components'
 
 export const Button = styled.button`
-    margin: 0 1em;
-    border-radius: 3px;
-    padding: 0.25em 1em;
+    font-size: 0.875em;
+    border-radius: 0.250em;
+    border: none;
+    padding: 0 1em 0 1em;
+    width: 6em;
+    height: 2.25em;
     font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
     Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
-    border: none;
+    text-align: center;
 `;
+
+
+// 16px base; 64px = 4em, 36px = 2.25em, 4px = 0.250em, 14px = 0.875em
+
+// Contained Button -
+// min-width: 64dp
+// height: 36dp
+// padding: 16dp each side
+// corners rounded: 4dp
+// elevation: 2dp
+// font-size: 14dp
+
+// Contained Button w/ Icon -
+// padding before icon: 12dp
+// padding after icon before text: 8dp
+
+//@media only screen and (min-width: 768px)
+// For everything bigger than 768px ^
